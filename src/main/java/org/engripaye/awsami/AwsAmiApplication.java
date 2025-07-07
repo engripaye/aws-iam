a5f95e9989e2f90919e68f40c40a4e77032c2dd1
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-07-07 09:03:01 +0100

    iam first commit

---
+++ src/main/java/org/engripaye/awsami/AwsAmiApplication.java
@@ -0,0 +1,13 @@
+package org.engripaye.awsami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsAmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsAmiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/engripaye/awsami/AwsAmiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.engripaye.awsami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsAmiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
